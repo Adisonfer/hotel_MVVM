5dfcdf93753082496ae966e4d289aefc127c3771
Efremov Alexandr <sasha_efremov123@mail.ru>
2023-11-05 00:31:53 +0300

    Test

--- Test/ViewModels/MainViewModel.cs
+++ Test/ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
-﻿using hotel_MVVM.ViewModels.Base;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace hotel_MVVM.ViewModels
 {
-    internal class MainViewModel : ViewModel
+    internal class MainViewModel
     {
     }
 }
